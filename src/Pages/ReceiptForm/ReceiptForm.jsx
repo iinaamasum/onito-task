f17dc2ef55518e388560055cb696bbe5b480bfa0
iinaamasum <iinaamasum@gmail.com>
2022-07-29 04:18:10 +0600

    submit and cancel btn added along with remark input

--- src/Pages/ReceiptForm/ReceiptForm.jsx
+++ src/Pages/ReceiptForm/ReceiptForm.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BiCommand } from 'react-icons/bi';
 
 const ReceiptForm = () => {
   return (
@@ -6,6 +7,7 @@ const ReceiptForm = () => {
       <div className="container mx-auto px-5 md:px-7 lg:px-10 max-w-[1300px]">
         <h2 className="underline text-xl font-bold">Receipt Details</h2>
         <form>
+          {/* date  */}
           <div className="w-full md:w-[75%] lg:w-[60%] flex justify-between mt-5">
             <label className="w-1/4 required" htmlFor="date">
               Date
@@ -16,21 +18,23 @@ const ReceiptForm = () => {
               placeholder="Enter Date"
             />
           </div>
+          {/* amount */}
           <div className="w-full md:w-[75%] lg:w-[60%] flex justify-between mt-2">
             <label className="w-1/4 required" htmlFor="amount">
               Amount
             </label>
             <input
               className="w-3/4 rounded py-1 px-2 bg-gray-100 focus:outline-none"
-              type="text"
+              type="number"
               placeholder="Enter Amount (in INR)"
             />
           </div>
+          {/* payment method */}
           <div className="w-full md:w-[75%] lg:w-[60%] flex justify-between mt-2">
             <label className="w-1/4 required" htmlFor="payment-method">
               Payment Method
             </label>
-            <select class="select select-secondary w-3/4 rounded px-2 bg-gray-100 focus:outline-none border-none max-h-9 min-h-6">
+            <select class="select select-secondary w-3/4 rounded px-2 bg-gray-100 focus:outline-none border-none max-h-8 min-h-6">
               <option selected>Cash</option>
               <option>Google Pay</option>
               <option>Bank Payment</option>
@@ -38,6 +42,32 @@ const ReceiptForm = () => {
               <option>Card</option>
               <option>Mobile Banking</option>
             </select>
+          </div>
+          {/* remark */}
+          <div className="w-full md:w-[75%] lg:w-[60%] flex justify-between mt-2">
+            <label className="w-1/4" htmlFor="remark">
+              Remark
+            </label>
+            <input
+              className="w-3/4 rounded py-1 px-2 bg-gray-100 focus:outline-none"
+              type="text"
+              placeholder="Enter Remark"
+            />
+          </div>
+          <div className="w-full md:w-[75%] lg:w-[60%] flex justify-between mt-5">
+            <label className="w-1/4" htmlFor=""></label>
+            <div className="flex justify-around items-center w-full">
+              <button className="border-2 border-red-500 rounded px-10 py-2 text-lg font-medium uppercase text-red-500 hover:text-black hover:bg-red-500 flex flex-col">
+                <span>Cancel</span>
+                <span className="flex items-center">(ESC)</span>
+              </button>
+              <button className="bg-green-700 hover:bg-green-600 rounded px-10 py-2 text-lg font-medium uppercase text-white flex flex-col">
+                <span>Submit</span>
+                <span className="flex items-center">
+                  (<BiCommand /> + S)
+                </span>
+              </button>
+            </div>
           </div>
         </form>
       </div>
